f11bc05da2d98eeab837b6c9da18f3e1d0fce758
zch <1361934810@qq.com>
2020-10-19 17:30:09 +0800

    前端页面完成

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-export default {};
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
-<style >
-body,
-html {
-  margin: 0 !important;
-  padding: 0 !important;
-}
-.ql-editor {
-  min-height: 180px !important;
-  margin-left: 20px;
-}
-.quill-editor{
-  /* margin-left: 60px; */
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
